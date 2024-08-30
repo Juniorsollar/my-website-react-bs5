a4ce3a8757f3328a6c3d93b3d76eb9da39f25e61
Juniorsollar <alesollar2013@hotmail.com>
2024-08-29 23:55:00 -0300

    Primeiro commit do meu site react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <Navbar
+          expand="lg"
+          className="bg-body-tertiary"
+          bg="dark"
+          data-bs-theme="dark"
         >
-          Learn React
-        </a>
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col sm={6}>
+              <Image
+                src="https://media.licdn.com/dms/image/v2/C4E03AQFZUapwnxaV3w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654094228278?e=1730332800&v=beta&t=fQSPgRvkClvDqvurkSDSWW0rTK8OGd_CvuTcTuFv0hc"
+                fluid
+                roundedCircle
+                className=""
+              />
+            </Col>
+            <Col sm={6}>
+              <h1 class="font-weight-light">Alessandro Sollar Jr.</h1>
+              <p class="mt-4">
+                Desenvolvedor de Software - FullStack | Engenheiro de Computação
+                | JavaScript | TypeScript | React.JS | Node.js | Bootstrap |
+                MySQL | Git | Docker
+              </p>
+              <Button
+                href="https://www.linkedin.com/in/alessandrosollarjr/"
+                variant="outline-primary"
+              >
+                {" "}
+                E-mail{" "}
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Card className="text-center bg-secondary text-white my-5 py-4">
+              <Card.Body>This is some text within a card body.</Card.Body>
+            </Card>
+          </Row>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  style={{
+                    width: "100%",
+                    height: "250px",
+                    objectFit: "scale-down",
+                  }}
+                  fluid
+                  roundedCircle
+                  variant="top"
+                  src="https://static.vecteezy.com/system/resources/previews/018/930/480/non_2x/linkedin-logo-linkedin-icon-transparent-free-png.png"
+                />
+                <Card.Body>
+                  <Card.Title>LinkedIn</Card.Title>
+                  <Card.Text>
+                    Conecte-se comigo no LinkedIn para acompanhar minhas
+                    atualizações profissionais, discutir novas ideias e explorar
+                    oportunidades de colaboração.
+                  </Card.Text>
+                  <Button
+                    href="https://www.linkedin.com/in/alessandrosollarjr/"
+                    variant="outline-primary"
+                  >
+                    LinkedIn
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  style={{
+                    width: "100%",
+                    height: "250px",
+                    objectFit: "scale-down",
+                  }}
+                  fluid
+                  roundedCircle
+                  variant="top"
+                  src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png"
+                />
+                <Card.Body>
+                  <Card.Title>GitHub</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  style={{
+                    width: "100%",
+                    height: "250px",
+                    objectFit: "scale-down",
+                  }}
+                  fluid
+                  roundedCircle
+                  variant="top"
+                  src="https://www.creativefabrica.com/wp-content/uploads/2018/11/Hardware-Logo-by-Friendesign-Acongraphic-27-580x386.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Consultoria de Hardwares</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <footer class="py-5 my-5 bg-dark">
+        <container className="px-4">
+          <p class="text-center text-white">
+            Copyright &copy; Alessandro Sollar Jr. 2024
+          </p>
+        </container>
+      </footer>
     </div>
   );
 }
