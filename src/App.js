fc3641469fb89d7dad259c9dc91033e729b6d0fc
Alessandro Antonio Sollar Junior <57305761+Juniorsollar@users.noreply.github.com>
2024-11-05 15:33:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Row className="px-4 my-5">
             <Col sm={6}>
               <Image
-                src="https://media.licdn.com/dms/image/v2/C4E03AQFZUapwnxaV3w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654094228278?e=1730332800&v=beta&t=fQSPgRvkClvDqvurkSDSWW0rTK8OGd_CvuTcTuFv0hc"
+                src="https://media.licdn.com/dms/image/v2/C4E03AQFZUapwnxaV3w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654094228278?e=1736380800&v=beta&t=8-cQLMa_pl3GtK0m8uRcWwcdXL9E9BDciRyK3bTI_xo"
                 fluid
                 roundedCircle
                 className=""
