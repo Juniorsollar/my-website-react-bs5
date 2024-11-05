6fb368fcda5174d799105ea9d8bed99e43f2ccdb
Alessandro Antonio Sollar Junior <57305761+Juniorsollar@users.noreply.github.com>
2024-11-05 16:01:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ function App() {
                 <Card.Body>
                   <Card.Title>Consultoria de Hardwares</Card.Title>
                   <Card.Text>
-                    Consultoria para montagem de computadores! Orientação completa para escolher os componentes ideais, garantir desempenho e qualidade, além de suporte personalizado.
+                    Orientação completa para escolher os componentes ideais, garantir desempenho e qualidade, além de suporte personalizado.
                   </Card.Text>
                   <Button 
                     href = "https://www.instagram.com/alessandrosollar/"
